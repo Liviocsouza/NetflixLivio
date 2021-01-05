c31613af4a45e86d78fa6c15d1ad59c025cda4d7
Liviocsouza <liviocavalcantid@gmail.com>
2021-01-04 22:43:12 -0300

    site structure

--- src/components/FeatureMovie/index.js
+++ src/components/FeatureMovie/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './styles.css';
 
 export default ({item})=>{
+    let firsDate = new Date(item.first_air_date);
+    let genres = [];
+    for(let i in item.genres){
+        genres.push(item.genres[i].name);
+    }
+
+
     return(
         <section className="featured" style={{
             backgroundSize:'cover',
@@ -9,7 +16,19 @@ export default ({item})=>{
             backgroundImage:`url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
         }}> 
             <div className="featured--vertical">
-                <div className="fetured--horizontal">
+                <div className="featured--horizontal">
+                    <div className="featured--name">{item.original_name}</div>
+                    <div className="featured--info">
+                        <div className="featured--points">{item.vote_average} pontos</div>
+                        <div className="featured--year"> {firsDate.getFullYear()}</div>
+                        <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's': ''}</div>
+                    </div>
+                    <div className="featured-desciption">{item.overview}</div>
+                    <div className="featured-buttons">
+                            <a href = {`/watch/${item.id}`}>► Assistir</a>
+                            <a href ={`/list/add/${item.id}`}>+ Minha Lista</a>
+                    </div>
+                    <div className="featured--genres"><strong>Gêneros:</strong>{genres.join(', ')}</div>
 
                 </div>
 
